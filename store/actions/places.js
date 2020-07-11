fe87631a0a4f6bc9e958a4c0f340a6bd36eb419d
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-07-11 11:33:14 +0100

    add image to filesystem

--- store/actions/places.js
+++ store/actions/places.js
@@ -1,8 +1,25 @@
+import * as FileSystem from "expo-file-system";
+
 import { ADD_PLACE } from "../types";
 
 export const addPlace = (title, image) => {
-  return {
-    type: ADD_PLACE,
-    payload: { title, image },
+  return (dispatch) => {
+    const fileName = image.split("/").pop();
+    const newPath = FileSystem.documentDirectory + fileName;
+
+    try {
+      FileSystem.moveAsync({
+        from: image,
+        to: newPath,
+      });
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+
+    dispatch({
+      type: ADD_PLACE,
+      payload: { title, image: newPath },
+    });
   };
 };
